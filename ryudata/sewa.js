a61ed297fc3be61fdacec46e3c533444fcf5c322
VELLBOTZ <96705763+VELLBOTZ@users.noreply.github.com>
2022-02-23 13:53:07 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -1,10 +1,10 @@
 exports.sewa = () => {
 	return`𝗢 𝗣 𝗘 𝗡  𝗦 𝗘 𝗪 𝗔  𝗕 𝗢 𝗧
 
-• *1 Minggu : 5.000 ( 5K )*
-• *1 Bulan : 10.000 ( 10K )*
-• *Permanen : 15.000 ( 15K )*
-• *Join Murid Bot : 20.000 ( 20K )*
+• *1 Minggu : 2.000 ( 2K )*
+• *1 Bulan : 6.000 ( 6K )*
+• *Permanen : 10.000 ( 10K )*
+• *Join Murid Bot : Ga nerima*
 
 Minat Ketik .owner
 `
